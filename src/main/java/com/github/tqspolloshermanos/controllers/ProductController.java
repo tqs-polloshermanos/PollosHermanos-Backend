8a5fc943b7ec49189610cd17d74974a55ac80659
Mateusz Kubiak <matkubiak888@gmail.com>
2024-05-31 22:25:20 +0100

    Revert "refactor: remove 'api' from product endpoint addresses"
    This reverts commit dc8465d6fedac744ce9357aa8572e78e77f4525d.

--- src/main/java/com/github/tqspolloshermanos/controllers/ProductController.java
+++ src/main/java/com/github/tqspolloshermanos/controllers/ProductController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
